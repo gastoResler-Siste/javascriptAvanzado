b666d6b82f9ccb3fee682a2f24981344eb44a5df
gastoResler-Siste <resler.gaston@gmail.com>
2023-09-02 11:24:44 -0300

    xczxc

--- actividades1/actividades1.js
+++ actividades1/actividades1.js
@@ -5,35 +5,36 @@
 // edad y amigos, que es un array de objetos donde cada objeto 
 // representa un amigo con propiedades como nombre y edad. Agrega
 // al menos tres amigos y muestra en la consola el nombre del amigo más joven.
+function acti5(){
+    const perfil={
+        nombre:"juan",
+        edad: 45,
+        amigos:[
+            {
+                nombre:"fsqweqweqwed",
+                edad:"23"
+            },
+            {
+                nombre:"fwsd",
+                edad:"21"
+            },
+            {
+                nombre:"gggggggfsd",
+                edad:"27"
+            },
+        ]
 
-const perfil={
-    nombre:"juan",
-    edad: 45,
-    amigos:[
-        {
-            nombre:"fsqweqweqwed",
-            edad:"23"
-        },
-        {
-            nombre:"fwsd",
-            edad:"21"
-        },
-        {
-            nombre:"gggggggfsd",
-            edad:"27"
-        },
-    ]
+    };//fin perfil
 
-};//fin perfil
-
-let mas_joven=perfil.amigos[0];
-for(const amigo of perfil.amigos){
-    if (mas_joven.edad>amigo.edad){
-        mas_joven=amigo
+    let mas_joven=perfil.amigos[0];
+    for(const amigo of perfil.amigos){
+        if (mas_joven.edad>amigo.edad){
+            mas_joven=amigo
+        }
     }
-}
-console.log("el amigo mas joven " + mas_joven.nombre )
-//ordenar
-perfil.amigos.short((a,b)=>  a.edad - b.edad);
-console.log(perfil.amigos[0])
+    console.log("el amigo mas joven " + mas_joven.nombre )
+    //ordenar
+    //perfil.amigos.short((a,b)=>  a.edad - b.edad);
+   // console.log(perfil.amigos[0])
 
+}//cierro la funcion
