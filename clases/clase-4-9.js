6859c8819c3411918c3e781924474ed33708d195
gastoResler-Siste <resler.gaston@gmail.com>
2023-09-04 20:54:05 -0300

    Create clase-4-9.js

---
+++ clases/clase-4-9.js
@@ -0,0 +1,15 @@
+var numeros = [2,45,2,33,12];
+/*let suma = 0;
+for(let i = 0; i < numeros.length ; i++) {
+  suma += numeros[i];
+*/
+
+
+//reducce es una funcion de Js
+//SumAcumulativa es una variable que yo menciono para que se acumule ahi la suma del arreglo
+//elem hace referencia a los elementos del arreglo
+//0 es el valor de inicio
+
+suma = numeros.reduce((sumAcumulativa,elem) => sumAcumulativa + elem, 0);
+console.log("El resultado es: " + suma);
+
