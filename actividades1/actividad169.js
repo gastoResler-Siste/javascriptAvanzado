98ada386789db90fd655ece8556be31e943b115a
gastoResler-Siste <resler.gaston@gmail.com>
2023-09-16 10:35:53 -0300

    asdfgh

---
+++ actividades1/actividad169.js
@@ -0,0 +1,15 @@
+//La tienda vendera productos que tendran propiedades como nombre, 
+//precio y cantidad en stock. Incluye m�todos para mostrar la informaci�n
+// del producto.
+
+//Crea dos tipos de productos. Por ejemplo, puedes tener una clase 
+//ProductoElectr�nico y una clase ProductoAlimenticio. 
+//Cada clase hija debe tener propiedades espec�ficas 
+//(por ejemplo, potencia para productos electr�nicos y fecha de caducidad
+// para productos alimenticios) y m�todos relacionados con su tipo de 
+//producto.
+
+//Crea una clase llamada Carrito que tenga un array para almacenar los 
+//productos seleccionados por el usuario. Esta clase debe incluir m�todos 
+//para agregar un producto al carrito, eliminar un producto del carrito y 
+//calcular el total de la compra.
