bbd9cdd7a2b8e952ff3034a16ff5ecff1eab7366
gastoResler-Siste <resler.gaston@gmail.com>
2023-09-21 17:42:47 -0300

    sd

--- actividades1/tp1.js
+++ actividades1/tp1.js
@@ -2,3 +2,10 @@ let numeros=[10,15,18,6,8,78,35];
 function tp1Act1(){
     
 }
+//reducce es una funcion de Js
+//SumAcumulativa es una variable que yo menciono para que se acumule ahi la suma del arreglo
+//elem hace referencia a los elementos del arreglo
+//0 es el valor de inicio
+
+suma = numeros.reduce((sumAcumulativa,elem) => sumAcumulativa + elem, 0);
+console.log("El resultado es: " + suma);
